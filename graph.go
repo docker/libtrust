b991f29af15dbeea904cddb97e9fd2b1789f935f
Solomon Hykes <solomon@docker.com>
2014-05-28 18:47:33 -0700

    Graph: strawman interface for a global namespace with authorization and delegation

---
+++ graph.go
@@ -0,0 +1,42 @@
+package libtrust
+
+import ()
+
+type Graph interface {
+	Set(from, to string, cat Category) (Edge, error)
+	Get(id string) (Edge, error)
+	GetChildren(id string) ([]Edge, error)
+	Delete(Edge) error
+	DeleteAll(Edge) error
+	Walk(Edge, func(Edge))
+}
+
+type Edge interface {
+	From() string
+	To() string
+	Cat() Category
+	Action() Action
+	Neighbours(reverse bool, cat Category) ([]Edge, error)
+}
+
+type Category int
+
+const (
+	Authorize Category = iota
+	Delegate
+)
+
+type Action int
+
+const (
+	LS Action = iota
+	CD
+	CONNECT
+	SET
+	GET
+	LOG
+	STOP
+	START
+	RM
+	REGISTER
+)
